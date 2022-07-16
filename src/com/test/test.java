88cfb6089b3eed11c0a369a22703faa8069f4b8a
shy <651485890@qq.com>
2022-07-16 15:30:06 +0800

    测试

---
+++ src/com/test/test.java
@@ -0,0 +1,13 @@
+package com.test;
+
+/**
+ * @author 努力学习的程序员
+ * @Date 2022/7/16 15:27
+ * @Description
+ * @Version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("测试");
+    }
+}
