18e1b803e98c77f14eb2529e882bce446238b0f7
shy360360360 <49462896+shy360360360@users.noreply.github.com>
2022-07-16 15:32:06 +0800

    Update test.java

--- src/com/test/test.java
+++ src/com/test/test.java
@@ -9,6 +9,6 @@ package com.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("测试");
-        System.out.println("测试2");
+        System.out.println("");
     }
 }
